10145867fad9c5cc762233af8c91e4b1dd58f4c8
Amine Douiri <38985975+AminDouiri99@users.noreply.github.com>
2022-11-17 02:35:44 +0100

    Update Operateur.java

--- src/main/java/com/esprit/examen/entities/Operateur.java
+++ src/main/java/com/esprit/examen/entities/Operateur.java
@@ -34,7 +34,12 @@ public class Operateur implements Serializable{
 	@OneToMany
 	@JsonIgnore
 	private Set<Facture> factures;
-
+public Operateur(Long idOperateur, String nom, String prenom, String password) {
+		this.idOperateur = idOperateur;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.password = password;
+	}
 	public Operateur(String nom,String prenom, String password){
 		super();
 		this.nom=nom;
